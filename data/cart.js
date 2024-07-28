268b92a0a27891af97424ffbfe7cbe0058456100
aryan181004 <aryan2004.chavan@gmail.com>
2024-07-28 16:18:24 +0530

    delete item from cart using .remove() in dom

--- data/cart.js
+++ data/cart.js
@@ -29,3 +29,15 @@ export function addToCart(productId){
           });
         }
   }
+
+export function removeFromCart(productId){
+  let newCart = [];
+
+  cart.forEach((cartItem) => {
+    if(cartItem.productId !== productId){
+      newCart.push(cartItem);
+    }
+  });
+
+  cart = newCart;
+}
